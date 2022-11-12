61e0829a082ebee485701791ed52cb1ca2923693
kdeng55 <kdeng@bu.edu>
2022-11-12 17:56:39 -0500

    test3

--- src/App.js
+++ src/App.js
@@ -2,26 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    return (
+<div className="App">
+ <header className="App-header">
+    Hello!!!
+</header>
 
-      <p>He</p>
-    </div>
-  );
+<p className="App-p">He</p>
+    </div>
+  );
 }
+
 
 export default App;
