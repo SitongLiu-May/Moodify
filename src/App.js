75aac68fbf8c255413a68743c2ec14b1c22d9de7
mayliu <mayliu@bu.edu>
2022-11-12 19:27:01 -0500

    changed things

--- src/App.js
+++ src/App.js
@@ -16,9 +16,19 @@ return (
 <p className = "Emotions">Sad</p>
 <p className = "Emotions">Dead</p>
 
+<p className="App-p">Welcome to Moodify. We're here to help you</p>
+
+<p> Please tell us hofhfhw you feel hah husakhskahkhadw</p>
 </div>
 );
 }
+const genreNames = ['Action', 'Adventure', 'Animation', 'Comedy', 
+  'Documentary', 'Drama', 'Family', 'Fantasy',
+  'History', 'Horror', 'Music', 'Mystery',
+  'Romance', 'Science Fiction', 'TV Movie', 'Thriller',
+  'War', 'Western']
+
+
 
 
 export default App;
