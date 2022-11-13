64764dd1fb778b7edd94eea3d47fe8206ed371f2
allisonlu8 <98787033+allisonlu8@users.noreply.github.com>
2022-11-12 19:03:38 -0500

    first page text

--- src/App.js
+++ src/App.js
@@ -4,9 +4,18 @@ import './App.css';
 function App() {
 return (
 <div className="App">
-<p className="App-p">Welcome to Moodify. We're here to help you</p>
+<p className="App-WelcomeGreating">Welcome to Moodify. We're here to help you</p>
+<p className = "App-WelcomeGreating">Hello.</p>
 
-<p> Please tell us hofhfhw you feel hah husakhskahkhadw</p>
+
+<p className = "Heading">How do you feel?</p>
+<p className = "Good">Good morning</p>
+
+<p className = "Emotions">Happy</p>
+<p className = "Emotions">Angry</p>
+<p className = "Emotions">Sad</p>
+<p className = "Emotions">Dead</p>
+
 </div>
 );
 }
